aa1c703d7422a5eea8901d922ee056439981c7d7
MoNRaGitTwo <rh343mariosdvcut33@hotmail.com>
2024-05-28 01:04:16 -0300

    al fin

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ function Productos() {
   const [productos, setProductos] = useState([]);
 
   useEffect(() => {
-    // Realizar la solicitud HTTP al endpoint del backend
-    fetch('https://f2d3-2800-a4-1114-9400-bc2e-8694-c4a9-f967.ngrok-free.app/TodoProductos')
+    // URL del backend expuesta a través de Ngrok
+    const ngrokUrl = 'https://fb2b-2800-a4-119b-6900-bc2e-8694-c4a9-f967.ngrok-free.app/Products/TodoProductos';
+
+    // Realizar la solicitud HTTP al endpoint del backend con el encabezado para omitir la advertencia de Ngrok
+    fetch(ngrokUrl, {
+      headers: {
+        'ngrok-skip-browser-warning': 'true' // Agregar este encabezado
+      }
+    })
       .then(response => {
+        console.log('Status:', response.status); // Imprimir el estado de la respuesta
+        console.log('Headers:', response.headers); // Imprimir los encabezados de la respuesta
+
         if (!response.ok) {
-          throw new Error('Error al obtener productos');
+          throw new Error(`Error al obtener productos: ${response.statusText}`);
         }
-        return response.json();
+        return response.json(); // Obtener la respuesta como JSON
       })
       .then(data => {
-        // Procesar la respuesta y establecer los productos en el estado
+        console.log('Response data:', data); // Imprimir la respuesta del servidor
         setProductos(data);
       })
       .catch(error => {
