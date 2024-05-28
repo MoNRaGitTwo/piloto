f43b922578b7c75ad70c32347c9ae5d515a33b19
MoNRaGitTwo <rh343mariosdvcut33@hotmail.com>
2024-05-28 00:10:17 -0300

    sdfs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import AddProductForm from './componentes/CrearProducto'
-
+import AddProductForm from './componentes/CrearProducto';
 
 function Productos() {
   const [productos, setProductos] = useState([]);
 
   useEffect(() => {
     // Realizar la solicitud HTTP al endpoint del backend
-    fetch('https://e690-2800-a4-1114-9400-bc2e-8694-c4a9-f967.ngrok-free.app/TodoProductos')
+    fetch('https://f2d3-2800-a4-1114-9400-bc2e-8694-c4a9-f967.ngrok-free.app/TodoProductos')
       .then(response => {
         if (!response.ok) {
-          throw new Error('Error');
+          throw new Error('Error al obtener productos');
         }
         return response.json();
       })
@@ -19,14 +18,14 @@ function Productos() {
         setProductos(data);
       })
       .catch(error => {
-        console.error('Error al obtener pdroductos:', error);
+        console.error('Error al obtener productos:', error);
       });
   }, []); // Se ejecutará solo una vez al cargar el componente
 
   return (
     <div>
       <h1>Productos</h1>
-      <AddProductForm></AddProductForm>
+      <AddProductForm />
       <ul>
         {productos.map(producto => (
           <li key={producto.Id}>
@@ -35,8 +34,6 @@ function Productos() {
         ))}
       </ul>
     </div>
-
-      
   );
 }
 
