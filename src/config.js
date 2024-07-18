5dd38daf14b0dbd3f676c30d8182f482e7ebe415
MoNRaGitTwo <ju4nrsuarez@gmail.com>
2024-07-18 20:39:27 -0300

    urlx3

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@
 
 
 export const API_BASE_URL = '   https://backpiloto-3.onrender.com/Products';
-export const API_BASE_URL2 = '   https://396d-167-60-192-17.ngrok-free.app/clientes';
-export const API_BASE_URL3 = '   https://396d-167-60-192-17.ngrok-free.app';
+export const API_BASE_URL2 = '   https://backpiloto-3.onrender.com/clientes';
+export const API_BASE_URL3 = '   https://backpiloto-3.onrender.com';
 
 
 
